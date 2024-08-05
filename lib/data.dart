7afba4dee733d34f15610df443cd7f42433a17f8
rizki <rizkiadnanfutuh@gmail.com>
2024-08-05 22:40:32 +0700

    feat : add assets

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+class product {
+  final String name;
+  final String price;
+  final String quantity;
+  final String image;
+  final String description;
+
+  product(
+    this.name,
+    this.price,
+    this.quantity,
+    this.image,
+    this.description,
+  );
+}
